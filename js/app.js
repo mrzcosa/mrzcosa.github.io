eb067fe0dbc6755a94aecbe1bf50d83ed9a3ce87
Mark Russell Cosa <markrussellzulueta.cosa@bicol-u.edu.ph>
2026-06-13 01:23:37 +0800

    Initial portfolio upload

---
+++ js/app.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    new Typed("#typing", {
+
+        strings: [
+            "Aspiring Software Developer",
+            "Android Developer",
+            "ASP.NET Core Developer",
+            "Backend Developer",
+            "Cybersecurity Enthusiast"
+        ],
+
+        typeSpeed: 90,
+        backSpeed: 60,
+        backDelay: 1200,
+        loop: true,
+        showCursor: true,
+        cursorChar: "|"
+
+    });
+
+});
